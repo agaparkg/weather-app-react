3510e23d86c99af908f93a3896dd39ab742abe8d
Azamat Gaparov <pennphil.ttzf@gmail.com>
2021-01-25 20:50:22 -0500

    modified AsideLeft component

--- src/components/AsideLeft.js
+++ src/components/AsideLeft.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { debounce } from "lodash";
+import ResultSection from "./ResultSection";
+import FormSubmit from "./FormSubmit";
 
 function AsideLeft() {
   const [weatherData, setWeatherData] = useState({
@@ -36,12 +38,6 @@ function AsideLeft() {
       .catch((err) => setError(err));
   };
 
-  const convertKtoF = (temp, type) => {
-    return type === "fahrenheit"
-      ? Math.floor(((temp - 273.15) * 9) / 5 + 32)
-      : Math.floor(temp - 273.15);
-  };
-
   const handleTempTypeChange = (e) => {
     setTempType(e.target.value);
   };
@@ -57,48 +53,13 @@ function AsideLeft() {
         placeholder="Enter city..."
       />
       {error && <div id="error">There is no such city in the world</div>}
-      <form onSubmit={handleFormSubmit}>
-        <input
-          checked={tempType === "fahrenheit"}
-          onChange={handleTempTypeChange}
-          type="radio"
-          id="fahrenheit"
-          name="degree"
-          value="fahrenheit"
-        />{" "}
-        <label htmlFor="fahrenheit">Fahrenheit</label>
-        <br />
-        <input
-          onChange={handleTempTypeChange}
-          type="radio"
-          id="celsius"
-          name="degree"
-          value="celsius"
-        />{" "}
-        <label htmlFor="celsius">Celsius</label>
-        <br />
-        <input id="submit" type="submit" value="Submit" />
-      </form>
+      <FormSubmit
+        handleFormSubmit={handleFormSubmit}
+        tempType={tempType}
+        handleTempTypeChange={handleTempTypeChange}
+      />
       {showResult && (
-        <section className="search-result">
-          <i className={`wi wi-owm-${weatherData.weather[0].id} display-1`}></i>
-          <div className="result-description">
-            <span>
-              {weatherData.name}, {weatherData.sys.country}
-            </span>
-            <h3>{convertKtoF(weatherData.main.temp, tempType)}&deg;</h3>
-            <h6>
-              <span>
-                Low: {convertKtoF(weatherData.main.temp_min, tempType)}&deg;
-              </span>
-              {" - "}
-              <span>
-                High: {convertKtoF(weatherData.main.temp_max, tempType)}&deg;
-              </span>
-            </h6>
-            <h4>{weatherData.weather[0].description}</h4>
-          </div>
-        </section>
+        <ResultSection weatherData={weatherData} tempType={tempType} />
       )}
     </aside>
   );
