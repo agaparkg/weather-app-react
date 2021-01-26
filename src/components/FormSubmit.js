59e4346cf749daf9a12e41ee4041e64b7e72b8ee
Azamat Gaparov <pennphil.ttzf@gmail.com>
2021-01-25 20:49:18 -0500

    created FormSubmit component

---
+++ src/components/FormSubmit.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function FormSubmit({ handleFormSubmit, tempType, handleTempTypeChange }) {
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <input
+        checked={tempType === "fahrenheit"}
+        onChange={handleTempTypeChange}
+        type="radio"
+        id="fahrenheit"
+        name="degree"
+        value="fahrenheit"
+      />{" "}
+      <label htmlFor="fahrenheit">Fahrenheit</label>
+      <br />
+      <input
+        onChange={handleTempTypeChange}
+        type="radio"
+        id="celsius"
+        name="degree"
+        value="celsius"
+      />{" "}
+      <label htmlFor="celsius">Celsius</label>
+      <br />
+      <input id="submit" type="submit" value="Submit" />
+    </form>
+  );
+}
+
+export default FormSubmit;
